cc6524301c279cc73bfcd3ccefcf43748327c0d4
Andrew Reynolds <areynold@illinois.edu>
2015-04-07 14:06:13 -0500

    Add instructor model

---
+++ MVC5-Sandbox/Models/Instructor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC5_Sandbox.Models
+{
+    public class Instructor
+    {
+        public Instructor()
+        {
+            this.Courses = new List<Course>();
+        }
+
+        // Primary Key
+        public int InstructorId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime HireDate { get; set; }
+
+        // Navigation Properties
+        public virtual ICollection<Course> Courses { get; private set; }
+    }
+}
