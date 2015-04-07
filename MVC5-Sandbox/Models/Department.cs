dcc7d1d3377ded6d26708a32fd7db690bdf7615a
Andrew Reynolds <areynold@illinois.edu>
2015-04-07 13:55:54 -0500

    Add department model

---
+++ MVC5-Sandbox/Models/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5_Sandbox.Models
+{
+    public class Department
+    {
+        /// <summary>
+        /// Department constructor sets up Courses navigation property
+        /// </summary>
+        public Department()
+        {
+            this.Courses = new HashSet<Course>();
+        }
+
+        // Primary key
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public decimal Budget { get; set; }
+        public System.DateTime StartDate { get; set; }
+        public int? Administrator { get; set; }
+
+        // Navigation Property
+        public virtual ICollection<Course> Courses { get; private set; } 
+    }
+}
