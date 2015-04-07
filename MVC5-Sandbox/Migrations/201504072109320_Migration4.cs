08df68fe37f02ac05df11465b04728829ab4edc5
Andrew Reynolds <areynold@illinois.edu>
2015-04-07 16:09:58 -0500

    update db

---
+++ MVC5-Sandbox/Migrations/201504072109320_Migration4.cs
@@ -0,0 +1,16 @@
+namespace MVC5_Sandbox.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Migration4 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
