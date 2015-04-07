ee7fab86bed8bfa9ddb00375af616669813aa049
Andrew Reynolds <areynold@illinois.edu>
2015-04-07 14:02:49 -0500

    Add details model

---
+++ MVC5-Sandbox/Models/Details.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MVC5_Sandbox.Models
+{
+    public class Details
+    {
+        public DateTime Time { get; set; }
+        public string Location { get; set; }
+        public string Days { get; set; }
+    }
+}
