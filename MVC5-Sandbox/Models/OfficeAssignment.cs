2501e119f2e09efa28ac8222396c95fb954963e4
Andrew Reynolds <areynold@illinois.edu>
2015-04-07 14:09:34 -0500

    add office assignment model

---
+++ MVC5-Sandbox/Models/OfficeAssignment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MVC5_Sandbox.Models
+{
+    public class OfficeAssignment
+    {
+        [Key()]
+        public Int32 InstructorId { get; set; }
+        public string Location { get; set; }
+
+        // When the Entity Framework sees Timestamp attribute 
+        // it configures ConcurrencyCheck and DatabaseGeneratedPattern=Computed.
+        [Timestamp]
+        public Byte[] Timestamp { get; set; }
+
+        // Navigation property
+        public virtual Instructor Instructor { get; set; }
+    }
+}
