bcf69a21826630875f9a058285bcfb78189a0bf4
Andrew Reynolds <areynold@illinois.edu>
2015-04-08 09:32:45 -0500

    Create area for ef school

---
+++ MVC5-Sandbox/Areas/EntitySchool/EntitySchoolAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MVC5_Sandbox.Areas.EntitySchool
+{
+    public class EntitySchoolAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "EntitySchool";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "EntitySchool_default",
+                "EntitySchool/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
